fe0826badb332cd16a387aedf80c6020457e8d56
Per Lange Laursen <perlangelaursen@gmail.com>
2019-03-24 16:16:39 +0100

    Server is running

--- src/main/scala/com/pll/Main.scala
+++ src/main/scala/com/pll/Main.scala
@@ -35,7 +35,6 @@ object JsonMapper {
   }
 }
 
-
 case class Radio(alias : String, allowed_locations : Seq[String])
 case class Location(location : String)
 
@@ -44,60 +43,59 @@ object Main extends App with Directives {
   implicit val materializer = ActorMaterializer()
 
   val db = Database.forConfig("h2mem")
-  try {
-    val radios = TableQuery[Radios]
-    val locations = TableQuery[Locations]
+  val radios = TableQuery[Radios]
+  val locations = TableQuery[Locations]
 
-    val setup = DBIO.seq((radios.schema ++ locations.schema).create)
-    val setupFuture = db.run(setup)
-    Await.result(setupFuture, Duration.Inf)
+  val setup = DBIO.seq((radios.schema ++ locations.schema).create)
+  val setupFuture = db.run(setup)
+  Await.result(setupFuture, Duration.Inf)
 
-    val route =
-      pathPrefix("radios"/ IntNumber) { id =>
-        pathEnd {
-          post {
-            entity(as[String]) { payload =>
-              val radio = JsonMapper.convert[Radio](payload)
-              radios += (id, radio.alias, None)
-              locations ++= (radio.allowed_locations.map((l) => {
-                (id, l)
-              }))
+  val route =
+    pathPrefix("radios"/ IntNumber) { id =>
+      pathEnd {
+        post {
+          entity(as[String]) { payload =>
+            val radio = JsonMapper.convert[Radio](payload)
+            radios += (id, radio.alias, None)
+            locations ++= (radio.allowed_locations.map((l) => {
+              (id, l)
+            }))
+            complete(StatusCodes.OK)
+          }
+        }
+      } ~ path("location") {
+        post {
+          entity(as[String]) { payload =>
+            val loc = JsonMapper.convert[Location](payload)
+            val checkIfLocationIsValid = locations.filter(_.rad_id === id).filter(_.location === loc.location)
+            val checkResult = Await.result(db.run(checkIfLocationIsValid.result), Duration.Inf)
+            if (checkResult.size == 1) {
+              val updateLocation = radios.filter(_.rad_id === id).map(_.location)
+              val updateAction = updateLocation.update(Some(loc.location))
+              db.run(updateAction)
               complete(StatusCodes.OK)
+            } else {
+              complete(403 -> "Forbidden location")
             }
           }
-        } ~ path("location") {
-          post {
-            entity(as[String]) { payload =>
-              val loc = JsonMapper.convert[Location](payload)
-              val checkIfLocationIsValid = locations.filter(_.rad_id === id).filter(_.location === loc.location)
-              val checkResult = Await.result(db.run(checkIfLocationIsValid.result), Duration.Inf)
-              if (checkResult.size == 1) {
-                val updateLocation = radios.filter(_.rad_id === id).map(_.location)
-                val updateAction = updateLocation.update(Some(loc.location))
-                db.run(updateAction)
-                complete(StatusCodes.OK)
-              } else {
-                complete(403 -> "Forbidden location")
-              }
-            }
-          } ~ get {
-            val findLocation = radios.filter(_.rad_id === id).map(_.location)
-            val findResult = Await.result(db.run(findLocation.result), Duration.Inf)
-            if (findResult.size == 1 && !(findResult.head.isEmpty)) {
-              complete(JsonMapper.toJson(Location(findResult.head.get)))
-            } else {
-              complete(404 -> "NOT FOUND")
-            }
+        } ~ get {
+          val findLocation = radios.filter(_.rad_id === id).map(_.location)
+          val findResult = Await.result(db.run(findLocation.result), Duration.Inf)
+          if (findResult.size == 1 && !(findResult.head.isEmpty)) {
+            complete(JsonMapper.toJson(Location(findResult.head.get)))
+          } else {
+            complete(404 -> "NOT FOUND")
           }
         }
       }
-
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-    bindingFuture.onFailure {
-      case e:Exception => println(s"$e Failed to bind to localhost at port 8080")
     }
-    println(s"Server location: http://localhost:8080/\nPress RETURN to stop the server")
-    StdIn.readLine()
-    bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
-  } finally db.close
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+  bindingFuture.onFailure {
+    case e:Exception => println(s"$e Failed to bind to localhost at port 8080")
+  }
+  println(s"Server location: http://localhost:8080/\nPress RETURN to stop the server")
+  StdIn.readLine()
+  db.close
+  bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
 }
